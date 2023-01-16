6eaa02f3ec21a27ba3dab4ae137429b7ef9cf0a7
jack <jack@jacks-Laptop.local>
2023-01-16 14:06:40 -0500

    added java to file

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+var topics = ['html', 'css', 'git', 'javascript'];
+
+for (var x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML'){
+    console.log("lets study HTML!");
+} else if (topic === 'CSS') { 
+    console.log("lets study CSS!");
+} else if (topic === 'GIT'){
+    console.log("lets study GIT!");
+} else if (topic === 'Javascript'){
+    console.log("lets study Javascript!");
+} else{ 
+    console.log("please try again");
+}
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+var x = 0;
+for (var x = 0; x < shapes.length; x++){
+    console.log(shapes[x]);
+}
+*/
